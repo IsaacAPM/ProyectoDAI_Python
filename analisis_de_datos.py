c045409826586b53fc5f6fb0186a6807af8da412
Isaac Alejandro Pimentel Morales <isaacapimentelm@gmail.com>
2019-12-12 21:20:28 -0600

    Leidos y analizados los datos de crimen

--- analisis_de_datos.py
+++ analisis_de_datos.py
@@ -5,4 +5,16 @@ Created on Tue Dec 10 15:16:25 2019
 
 @author: ipiment
 """
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+#%%
+#En esta celda se lee el archivo de crimenes y se gurada en max_crime los datos de los 
+#tres paises con mas crimen
+crimes = pd.read_csv("SYB62_328_201904_Intentional Homicides and Other Crimes.csv")
+max_crime = pd.DataFrame()
+max_crime["Value"] = crimes["Value"].nlargest(3)
+max_crime = pd.merge(max_crime,crimes,on="Value")
+print(max_crime["Intentional homicides and other crimes"])
+#%%
